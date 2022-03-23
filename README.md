3c42ae3514b7caff595a0bd4b8eac213b844d3ee
Vossup <odin.lodemel@gmail.com>
2022-03-23 10:35:53 +0100

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# dcst1008_2022_groupno7
+
+Poject DCST1008
+
+To use our application make sure to have Node.js and git installed.
+Use npm install through terminal to dowload dependencies
+after use npm start to launch desktop application
+
+We are in it bois
---
+++ electron/main.js
@@ -0,0 +1,37 @@
+/**
+ * Starts the desktop application through the electron framework.
+ */
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
+// Reload application on changes in src folder
+const electronReload = require('./electron-reload')(path.join(__dirname, 'src'), {
+  electron: path.join(__dirname, 'node_modules/.bin/electron'),
+  ignored: /^.*\.(json|txt)$/,
+});
+
+let mainWindow;
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: { nodeIntegration: true, contextIsolation: false },
+  });
+
+  // Open Development Tools
+  mainWindow.openDevTools();
+
+  mainWindow.loadFile('public/html/index.html');
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+// To prevent crash on exit in MacOS
+app.on('will-quit', () => {
+  electronReload.close();
+});
